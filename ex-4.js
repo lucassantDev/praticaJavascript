bae2f7f8b3550f3b089b376eddb908d0c31065b3
Lucas Santana <lucasfdesantana095@gmail.com>
2025-05-13 09:26:45 -0300

    feat: 4º exercicio - 2025-05-13

---
+++ ex-4.js
@@ -0,0 +1,11 @@
+// função para descobrir se o número é positivo ou negativo
+
+function valorNumero(num){
+    if(num > 0){
+        console.log('Número positivo')
+    }else{
+        console.log('Número negativo')
+    }
+}
+
+valorNumero(90);
